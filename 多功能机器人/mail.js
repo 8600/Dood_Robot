f6f5061f697f4c3103ec0c5035601a432efb5c22
PUGE <my@owo.help>
2016-09-27 14:49:48 +0800

    增加收取邮件内容功能

--- 多功能机器人/mail.js
+++ 多功能机器人/mail.js
@@ -7,6 +7,7 @@ const send = require("./send");
 const storage = require("./storage");
 const data = new storage();
 const CryptoJS = require("crypto-js");
+const inspect = require('util').inspect;
 
 //查询数据库
 function findMongoDB(UserID,findJson,fun){
@@ -26,18 +27,58 @@ function findMongoDB(UserID,findJson,fun){
         }
     });
 }
+//切割字符串
+function cutString(original, before, after, index){
+    index=index||0;
+    if (typeof index === "number"){var P = original.indexOf(before, index);
+        if (P>-1) {
+            if (after) {var f = original.indexOf(after, P + 1);
+                if (f>-1) {return original.substring(P + before.toString().length, f);}
+                else {console.error("owo [在文本中找不到 参数三 "+after+"]");}
+            }else {return original.substring(P + before.toString().length);}
+        }else {console.error("owo [在文本中找不到 参数一 "+before+"]");}
+    }else{console.error("owo [sizeTransition:"+index+"不是一个整数!]");}
+}
+
+
+//取出消息
+function processing_message(UserID,message, imap,mailNumber,sendMessage) {
+    let Number = mailNumber;
+    message.on('message', function (msg) {
+        msg.on('body', function (stream, info) {
+            //正文内容的处理
+            let buffer = ""; Number--;
+            if (info.which === 'TEXT') { console.log ('邮件内容:'+inspect(info.which)+', 总长度：'+info.size); }
+            stream.on('data', function (chunk) { buffer += chunk.toString('utf8');});
+            stream.once('end', function () {
+                if (info.which !== 'TEXT') { 
+                    const mailHeader = inspect(Imap.parseHeader(buffer));//邮件头部
+                    //分离出发件人和收件人
+                    sendMessage += "发件人:"+cutString(mailHeader,"from: [ "," ]")+"\r\n主题:"+cutString(mailHeader,"subject: [ "," ]")+"\r\n----------------------------------\r\n";
+                    if(Number<1){
+                        sendMessage = sendMessage.replace(/"/g,"");
+                        send.sendToDood(UserID,sendMessage,data.access_token);
+                    }
+                }
+                else console.log('消息主体: [%s] 加载完毕', inspect(info.which));
+            });
+        });
+    });
+
+    message.once('error', function (err) {
+        console.log('关闭邮件发生错误: ' + err);
+    });
+
+    message.once('end', function () {
+        imap.end();
+    });
+}
 
 //接收邮件处理进程receive mail
 function receive(UserID,account,password,beSureToRemind) {
     //处理消息的函数
     function receive_mail() {
-        const imap = new Imap({
-            user: account,
-            password: password,
-            host: "imap." + account.substring(account.indexOf("@")+1),
-            port: 993,
-            tls: parseInt(account)
-        });
+        const imap = new Imap({ user: account, password: password,host: "imap." + account.substring(account.indexOf("@")+1),port: 993,tls: parseInt(account)});
         //第二个参数表示邮件的打开方式是否为只读
         function openInbox(cb) {imap.openBox('INBOX', true, cb);}
         imap.once('ready', function () {
@@ -47,7 +88,17 @@ function receive(UserID,account,password,beSureToRemind) {
                     imap.search(["UNSEEN"], function (err, results) {
                         if (err) send.sendToDood(UserID,"发生错误：" + err,data.access_token);
                         else{
-                            if (beSureToRemind||results.length) send.sendToDood(UserID,'未读邮件数: ' + results.length ,data.access_token);
+                            let mailNumber = results.length;
+                            let sendMessage = "未读邮件数："+mailNumber+"\r\n----------------------------------\r\n";
+                            if (beSureToRemind || mailNumber) {
+                                if (mailNumber){
+                                    const message = imap.fetch(results, { bodies: '', markSeen: true });
+                                    processing_message(UserID,message,imap,mailNumber,sendMessage);//对得到的数据处理
+                                }
+                                else{
+                                    send.sendToDood(UserID,'未读邮件数: ' + mailNumber ,data.access_token);
+                                }
+                            }
                         }
                     });
                 }
@@ -66,11 +117,13 @@ exports.emilUser = function (UserID) {
             //解密密码
             const password = CryptoJS.AES.decrypt(response[0].password, data.key).toString(CryptoJS.enc.Utf8);
             receive(UserID,response[0].account,password,true);
+
         }
         else send.sendToDood(UserID,data.needToBind,data.access_token);
     };
     //通过UserID找用户邮箱账号密码
     findMongoDB(UserID,{"UserID":UserID},fun);
+
 };
 
 //解除关联
